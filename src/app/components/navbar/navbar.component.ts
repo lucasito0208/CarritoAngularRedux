814f0d0911640d4f3fbeb89042215512919fc363
Lucas <93831338+lucasito0208@users.noreply.github.com>
2024-08-18 01:16:27 +0200

    Ultimos swal del botón de eliminar producto

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CartItem } from '../../models/cartItem';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'navbar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+
+  @Input() items: CartItem[] = [];
+
+}
